24c2a24abe874b62dbdeb5d3ca89c1d9ed87c17c
jeanthatoneguy <juanantoniosalcedagarcia@gmail.com>
2025-09-24 20:57:36 +0100

    first commit

---
+++ prueba.js
@@ -0,0 +1,7 @@
+
+    function sumar() {
+      let a = Number(document.getElementById("n1").value);
+      let b = Number(document.getElementById("n2").value);
+      document.getElementById("resultado").textContent = a + b;
+    }
+
